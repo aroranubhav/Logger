40725b6bd572757e4fb631d8d59f89c6e4cd4de8
maxi <arora.anubhav93@gmail.com>
2025-06-26 16:55:03 +0530

    Add constants

---
+++ logger/src/main/java/com/maxi/logger/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.maxi.logger.util
+
+internal object Constants {
+
+    const val LOGGER_DATABASE = "logger_database"
+    const val VERBOSE_LOGS_TABLE = "verbose_logs"
+    const val DEBUG_LOGS_TABLE = "debug_logs"
+    const val INFO_LOGS_TABLE = "info_logs"
+    const val WARNING_LOGS_TABLE = "warning_logs"
+    const val ERROR_LOGS_TABLE = "error_logs"
+}
