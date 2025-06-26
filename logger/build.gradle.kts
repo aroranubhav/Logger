8b0e4b5bbb5c52baa8e60ca813d8e1356cf5cdcc
maxi <arora.anubhav93@gmail.com>
2025-06-26 16:53:47 +0530

    Add required dependencies

--- logger/build.gradle.kts
+++ logger/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -37,6 +38,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+    //room db
+    implementation(libs.room)
+    ksp(libs.room.compiler)
+
+    //gson
+    implementation(libs.gson.converter)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
