24b8435f35aea5fb8725958b6686911c6e85c6a8
maxi <arora.anubhav93@gmail.com>
2025-06-27 13:00:29 +0530

    Add constants

--- logger/src/main/java/com/maxi/logger/util/Constants.kt
+++ logger/src/main/java/com/maxi/logger/util/Constants.kt
@@ -8,4 +8,9 @@ internal object Constants {
     const val INFO_LOGS_TABLE = "info_logs"
     const val WARNING_LOGS_TABLE = "warning_logs"
     const val ERROR_LOGS_TABLE = "error_logs"
+    const val VERBOSE_LOGS_FILE_NAME_PREFIX = "verbose_logs_"
+    const val DEBUG_LOGS_FILE_NAME_PREFIX = "debug_logs_"
+    const val INFO_LOGS_FILE_NAME_PREFIX = "info_logs_"
+    const val WARNING_LOGS_FILE_NAME_PREFIX = "warning_logs_"
+    const val ERROR_LOGS_FILE_NAME_PREFIX = "error_logs_"
 }
