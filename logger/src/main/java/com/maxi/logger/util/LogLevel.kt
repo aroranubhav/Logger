8e84f53bae5bb10eb5517e7fdc2a48fc601a2e7f
maxi <arora.anubhav93@gmail.com>
2025-06-27 13:00:43 +0530

    Update enum class name

--- logger/src/main/java/com/maxi/logger/util/LogLevel.kt
+++ logger/src/main/java/com/maxi/logger/util/LogLevel.kt
@@ -2,7 +2,9 @@ package com.maxi.logger.util
 
 internal object LogLevel {
 
-    enum class ICONS(val icon: String) {
+    enum class Icon(
+        val icon: String
+    ) {
         VERBOSE("🔍"),
         DEBUG("🐞"),
         INFO("ℹ️"),
