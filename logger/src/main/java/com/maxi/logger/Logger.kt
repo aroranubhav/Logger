a71e2f138d75f66a9d1620a77bd2d4b1bd023ea0
maxi <arora.anubhav93@gmail.com>
2025-06-27 13:02:46 +0530

    Update logger class to write to db and file
    This commit changes the signature of the Logger instance method to take max file size, and write to file system, additionally contains the logic to write to db and files

--- logger/src/main/java/com/maxi/logger/Logger.kt
+++ logger/src/main/java/com/maxi/logger/Logger.kt
@@ -2,83 +2,178 @@ package com.maxi.logger
 
 import android.content.Context
 import android.util.Log
+import com.google.gson.Gson
 import com.maxi.logger.data.local.LoggerDatabase
 import com.maxi.logger.data.local.model.Debug
 import com.maxi.logger.data.local.model.Error
 import com.maxi.logger.data.local.model.Info
 import com.maxi.logger.data.local.model.Verbose
 import com.maxi.logger.data.local.model.Warning
+import com.maxi.logger.util.Constants.DEBUG_LOGS_FILE_NAME_PREFIX
+import com.maxi.logger.util.Constants.ERROR_LOGS_FILE_NAME_PREFIX
+import com.maxi.logger.util.Constants.INFO_LOGS_FILE_NAME_PREFIX
+import com.maxi.logger.util.Constants.VERBOSE_LOGS_FILE_NAME_PREFIX
+import com.maxi.logger.util.Constants.WARNING_LOGS_FILE_NAME_PREFIX
+import com.maxi.logger.util.DefaultDispatcherProvider
+import com.maxi.logger.util.DispatcherProvider
 import com.maxi.logger.util.LogLevel
+import com.maxi.logger.util.Utils
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import java.io.File
 
 class Logger private constructor(
-    context: Context
+    context: Context,
+    private val maxFileSize: Long,
+    private val writeToFile: Boolean
 ) {
 
+    private var appContext: Context
     private var loggerDb: LoggerDatabase
+    private var dispatcherProvider: DispatcherProvider
+    private val job = Job()
+    private val scope: CoroutineScope
 
     init {
-        val appContext = context.applicationContext
+        appContext = context.applicationContext
         loggerDb = LoggerDatabase.getInstance(appContext)
+        dispatcherProvider = DefaultDispatcherProvider()
+        scope = CoroutineScope(dispatcherProvider.io + job)
     }
 
     companion object {
         @Volatile
         private var INSTANCE: Logger? = null
 
-        fun getInstance(context: Context): Logger {
+        fun getInstance(
+            context: Context,
+            maxFileSize: Long,
+            writeToFile: Boolean = false
+        ): Logger {
             return INSTANCE ?: synchronized(this) {
-                INSTANCE ?: Logger(context.applicationContext).also {
+                INSTANCE ?: Logger(
+                    context.applicationContext,
+                    maxFileSize,
+                    writeToFile
+                ).also {
                     INSTANCE = it
                 }
             }
         }
     }
 
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.IO + job)
-
     fun v(tag: String, message: String) {
-        val logMessage = "${LogLevel.ICONS.VERBOSE.icon} $message"
+        val logMessage = "${LogLevel.Icon.VERBOSE.icon} $message"
         scope.launch {
-            loggerDb.verboseDao().insertLog(Verbose(message = logMessage))
+            if (writeToFile) {
+                val dao = loggerDb.verboseDao()
+                dao.insertLog(
+                    Verbose(
+                        message = logMessage,
+                        date = Utils.getCurrentDataTime()
+                    )
+                )
+                val logs = dao.getLogs().first()
+                val fileName = VERBOSE_LOGS_FILE_NAME_PREFIX + Utils.getCurrentDate() + ".txt"
+                writeToFile(logs, fileName)
+            }
         }
         Log.v(tag, logMessage)
     }
 
     fun d(tag: String, message: String) {
-        val logMessage = "${LogLevel.ICONS.DEBUG.icon} $message"
+        val logMessage = "${LogLevel.Icon.DEBUG.icon} $message"
         scope.launch {
-            loggerDb.debugDao().insertLog(Debug(message = logMessage))
+            if (writeToFile) {
+                val dao = loggerDb.debugDao()
+                dao.insertLog(
+                    Debug(
+                        message = logMessage,
+                        date = Utils.getCurrentDataTime()
+                    )
+                )
+                val logs = dao.getLogs().first()
+                val fileName = DEBUG_LOGS_FILE_NAME_PREFIX + Utils.getCurrentDate() + ".txt"
+                writeToFile(logs, fileName)
+            }
         }
         Log.d(tag, logMessage)
     }
 
     fun i(tag: String, message: String) {
-        val logMessage = "${LogLevel.ICONS.INFO.icon} $message"
+        val logMessage = "${LogLevel.Icon.INFO.icon} $message"
         scope.launch {
-            loggerDb.infoDao().insertLog(Info(message = logMessage))
+            if (writeToFile) {
+                val dao = loggerDb.infoDao()
+                dao.insertLog(
+                    Info(
+                        message = logMessage,
+                        date = Utils.getCurrentDataTime()
+                    )
+                )
+                val logs = dao.getLogs().first()
+                val fileName = INFO_LOGS_FILE_NAME_PREFIX + Utils.getCurrentDate() + ".txt"
+                writeToFile(logs, fileName)
+            }
         }
         Log.i(tag, logMessage)
     }
 
     fun w(tag: String, message: String) {
-        val logMessage = "${LogLevel.ICONS.WARN.icon} $message"
+        val logMessage = "${LogLevel.Icon.WARN.icon} $message"
         scope.launch {
-            loggerDb.warningDao().insertLog(Warning(message = logMessage))
+            if (writeToFile) {
+                val dao = loggerDb.warningDao()
+                dao.insertLog(
+                    Warning(
+                        message = logMessage,
+                        date = Utils.getCurrentDataTime()
+                    )
+                )
+                val logs = dao.getLogs().first()
+                val fileName = WARNING_LOGS_FILE_NAME_PREFIX + Utils.getCurrentDate() + ".txt"
+                writeToFile(logs, fileName)
+            }
         }
         Log.w(tag, logMessage)
     }
 
     fun e(tag: String, message: String) {
-        val logMessage = "${LogLevel.ICONS.ERROR.icon} $message"
+        val logMessage = "${LogLevel.Icon.ERROR.icon} $message"
         scope.launch {
-            loggerDb.errorDao().insertLog(Error(message = logMessage))
+            if (writeToFile) {
+                val dao = loggerDb.errorDao()
+                dao.insertLog(
+                    Error(
+                        message = logMessage,
+                        date = Utils.getCurrentDataTime()
+                    )
+                )
+                val logs = dao.getLogs().first()
+                val fileName = ERROR_LOGS_FILE_NAME_PREFIX + Utils.getCurrentDate() + ".txt"
+                writeToFile(logs, fileName)
+            }
         }
         Log.e(tag, logMessage)
+    }
+
+    private fun <T> writeToFile(logs: List<T>, fileName: String) {
+        val gson = Gson()
+        val logsString = gson.toJson(logs)
+
+        scope.launch {
+            val file = File(
+                appContext.filesDir,
+                fileName
+            )
+
+            if (file.exists()) {
+                file.writeText("") //clear file contents
+            }
+            file.appendText("$logsString \n")
+        }
     }
 
     fun close() {
@@ -86,3 +181,7 @@ class Logger private constructor(
         job.cancel()
     }
 }
+
+/**
+ * //TODO: enable, disable write to file,  file names option?
+ */
